ec8d13277dee29aee774202799d59d628faca3b8
Nimi1104 <joytemewei@gmail.com>
2022-04-07 21:13:52 +0100

    commit for task 56

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+*main - Entry point
+*
+*Return: Always 0 (success)
+*/
+int main(void)
+{
+printf ("Size of char: %d byte(s)\n", size of (char));
+printf ("Size of an init: %d byte (s)\n", size of (int));
+printf ("Size of a long int: %d byte (s)\n", size of (long int));
+printf ("Size of a long long int: %d byte (s)\n", size of (long long int));
+printf ("Size of a float: %d byte (s)\n", size of (float));
+return (0);
+}
