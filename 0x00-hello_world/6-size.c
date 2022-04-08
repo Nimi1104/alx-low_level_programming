3c126f4a3c9db6fad013ac90ebe1adc38d587b60
Nimi1104 <joytemewei@gmail.com>
2022-04-08 07:16:43 +0100

    commit for 6

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -5,7 +5,7 @@
  *
  *Return: Always 0 (Success)
  */
-int main(void)
+int main (void)
 {
 printf("Size of a char: %zu byte(s)\n", sizeof(char));
 printf("Size of an int: %zu byte(s)\n", sizeof(int));
