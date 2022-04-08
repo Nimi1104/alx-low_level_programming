d91405571018f3b7b39e8f6fb58d51da94b93d3a
Joy <joytemewei@gmail.com>
2022-04-08 23:38:04 +0100

    task 12

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -12,19 +12,19 @@ int num1, num2;
 
 for (num1 = 0; num1 <= 98; num1++)
 {
-for (num2 = num1 +1; num2 <= 99; num2++)
+for (num2 = num1 + 1; num2 <= 99; num2++)
 {
 putchar((num1 /10) + '0');
 putchar((num1 % 10) + '0');
-putchar('');
+putchar(' ');
 putchar((num2 / 10) + '0');
 putchar((num2 % 10) + '0');
 
-if (num 1 == 98 && num2 == 99)
+if (num1 == 98 && num2 == 99)
 continue;
 
 putchar(',');
-putchar('');
+putchar(' ');
 }
 }
 putchar('\n');
