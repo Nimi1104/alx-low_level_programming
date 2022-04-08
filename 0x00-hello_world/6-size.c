78f3b45308e9a65a0e5c641aab67ee98330146ca
Nimi1104 <joytemewei@gmail.com>
2022-04-08 06:45:47 +0100

    commit for task 6

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+
 /**
  *main - Entry point
  *
@@ -7,7 +8,7 @@
 int main(void)
 {
 printf("Size of char: %zu byte(s)\n", sizeof(char));
-printf("Size of an init: %zu byte(s)\n", sizeof(int));
+printf("Size of an int: %zu byte(s)\n", sizeof(int));
 printf("Size of a long int: %zu byte(s)\n", sizeof(long int));
 printf("Size of a long long int: %zu byte(s)\n", sizeof(long long int));
 printf("Size of a float: %zu byte(s)\n", sizeof(float));
