1a12849312f5e82935223d2a686001f73b4c952c
Joy <joytemewei@gmail.com>
2022-04-08 23:30:29 +0100

    task 101

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -12,6 +12,8 @@ int digit1, digit2, digit3;
 
 for (digit1 = 0; digit1 < 8; digit1++)
 {
+for (digit2 = digit1+1; digit2 <9; digit2++)
+{
 for (digit3 = digit2+1; digit3 < 10; digit3++)
 {
 putchar((digit1 % 10) + '0');
@@ -21,12 +23,12 @@ putchar((digit3 % 10) + '0');
 if (digit1 == 7 && digit2 == 8 && digit3 == 9)
 continue;
 
-putchar (',');
-putchar ('');
+putchar(',');
+putchar(' ');
 }
 }
 }
-putchar ('\n');
+putchar('\n');
 
 return (0);
 }
