b6aba4490e10bbea230b498892e18554b93f208f
Joy <joytemewei@gmail.com>
2022-04-08 23:23:28 +0100

    task 10

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -10,6 +10,8 @@ int main(void)
 {
 int digit1, digit2;
 
+for (digit1 = 0; digit1 < 9; digit1++)
+{
 for (digit2 = digit1 + 1; digit2 < 10; digit2++)
 {
 putchar ((digit1 % 10) + '0');
@@ -17,12 +19,12 @@ putchar ((digit2 % 10) + '0');
 
 if (digit1 == 8 && digit2 == 9)
 continue;
-putchar (',');
-putchar ('');
+putchar(',');
+putchar('');
 }
 }
 
-putchar ('\n");
+putchar('\n');
 
 return (0);
 }
