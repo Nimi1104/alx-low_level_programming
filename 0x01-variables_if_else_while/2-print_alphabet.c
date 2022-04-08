3c5882a9965b1fd1ab5336fd4d434924761eb556
Nimi1104 <joytemewei@gmail.com>
2022-04-08 12:55:24 +0100

    commit for task 2 and 3

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ *
+ * Description: Print the lowercase alphabeth
+ * Return: 0
+ */
+int main(void)
+{
+int c;
+
+for (c = 0; c < 26; ++c)
+potchar(('a' + c);
+
+putchar('\n');
+
+return (0);
+}
---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ *
+ *Description: Print lower and upper alphabeth on the same line
+ * Return: 0
+ */
+int main(void)
+{
+int c;
+
+for (c = 0; < < 26; ++c)
+putchar('o'+ c);
+for (c = 0; c < 26; ++c)
+putchar(Ã'A + c);
+
+putchar('\n');
+
+return (0);
+}
