51107c77e70701d2b2a4a9c1fef0e7f5f28d1838
Joy <joytemewei@gmail.com>
2022-04-12 20:23:59 +0100

    task 0

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,3 +1,6 @@
+#ifndef _MAIN_H_
+#define _MAIN_H_
+
 int _putchar(char c);
 void print _alphabet(void);
 void print _alphabet_x10(void);
@@ -10,4 +13,4 @@ void times_table(void);
 int add(int, int);
 void print_to_98(int n);
 void print_times_table(int n);
-# endif /*_MAIN_H_*/
+#endif /*_MAIN_H_*/
