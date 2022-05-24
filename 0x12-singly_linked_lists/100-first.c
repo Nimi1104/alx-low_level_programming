2d416f68669e6f1733882d06d60d028bab41b14e
Joy <joytemewei@gmail.com>
2022-05-24 22:30:32 +0100

    commit all

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -5,8 +5,8 @@
  * Return: no return.
  */
 
-void __attribute__((constructor)) bmain()
+void __attribute__ ((constructor)) bmain()
 {
-    printf("You're beat! and yet, you must allow");
-    printf(",\nI bore my house upon my back!\n");
+	printf("You're beat! and yet, you must allow");
+	printf(",\nI bore my house upon my back!\n");
 }
